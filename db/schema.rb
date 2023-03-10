fa696c633fd4bebd5213a099f816dcdcb12ae313
liliialyshchuk <liliia.lyshchuk@gamil.com>
2023-03-10 19:21:43 +0000

    add secure password

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_03_10_180901) do
+ActiveRecord::Schema.define(version: 2023_03_10_190832) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
@@ -21,13 +21,13 @@ ActiveRecord::Schema.define(version: 2023_03_10_180901) do
     t.integer "user_ud"
     t.integer "user_id"
   end
-  
 
   create_table "users", force: :cascade do |t|
     t.string "username"
     t.string "email"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "password_digest"
   end
 
 end
