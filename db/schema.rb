7b2fffdb6330b7785c551c8865f73bbf78f41118
liliialyshchuk <liliia.lyshchuk@gamil.com>
2023-03-15 20:44:07 +0000

    add user admin functionality

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_03_10_190832) do
+ActiveRecord::Schema.define(version: 2023_03_15_203630) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
@@ -28,6 +28,7 @@ ActiveRecord::Schema.define(version: 2023_03_10_190832) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "password_digest"
+    t.boolean "admin", default: false
   end
 
 end
